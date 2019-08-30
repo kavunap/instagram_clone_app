dacde04bc969229ee3666b923caafd928daa8264
kavunap <pkavuna@gmail.com>
2019-08-30 18:31:00 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'contacts/create'
   #get 'contacts/new'
-  get 'favorites/index'
-  get 'favorites/create'
-  get 'favorites/destroy'
+  #get 'favorites/index'
+  #get 'favorites/create'
+  #get 'favorites/destroy'
   devise_for :users
   root to: "posts#index"
   resources :favorites

4ac5155654230a7ed53ab812c3480292bee258da
kavunap <pkavuna@gmail.com>
2019-09-02 07:45:49 +0000

    foreign key remmoved for test reason

--- db/migrate/20190828170541_create_profiles.rb
+++ db/migrate/20190828170541_create_profiles.rb
@@ -2,7 +2,7 @@ class CreateProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :profiles do |t|
       t.string :image
-      t.references :user, foreign_key: true
+      
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -32,10 +32,8 @@ ActiveRecord::Schema.define(version: 2019_08_31_192000) do
 
   create_table "profiles", force: :cascade do |t|
     t.string "image"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,5 +52,5 @@ ActiveRecord::Schema.define(version: 2019_08_31_192000) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  add_foreign_key "profiles", "users"
+
 end

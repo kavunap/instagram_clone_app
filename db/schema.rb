453343ba920276e79835dd9f81ecc090ca4bd3e0
kavunap <pkavuna@gmail.com>
2019-09-02 06:30:03 +0000

    profiles_to_users connection

--- db/schema.rb
+++ db/schema.rb
@@ -54,5 +54,5 @@ ActiveRecord::Schema.define(version: 2019_08_31_192000) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
+  add_foreign_key "profiles", "users"
 end
